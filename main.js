fe5011069bb9cd020e636472658f69f7b99ebb16
Cathylle <59953419+Cathylle@users.noreply.github.com>
2020-06-12 00:26:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -377,9 +377,9 @@ else{
 				}
 
 		//refresh page on browser resize
-$(window).bind('resize', function(e)
-{
-  console.log('window resized..');
-  this.location.reload(false); /* false to get page from cache */
+//$(window).bind('resize', function(e)
+//{
+  //console.log('window resized..');
+ // this.location.reload(false); /* false to get page from cache */
   /* true to fetch page from server */
-});
+//});
